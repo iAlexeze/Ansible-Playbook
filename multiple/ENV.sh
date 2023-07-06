e7c4cf2e0d4b7646c2bbdc033a9510b681256f0e
ialexeze <ialexeze@gmail.com>
2023-07-06 05:11:46 +0100

    Ansible plays

---
+++ multiple/ENV.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Export ENV
+export WEB_PASSWORD="dckr_pat_O-ZLHxbIi6kNbAFz1BTzaLtoni8"
+export WEB2_PASSWORD="dckr_pat_O-ZLHxbIi6kNbAFz1BTzaLtoni8"
+export DB_PASSWORD="dckr_pat_O-ZLHxbIi6kNbAFz1BTzaLtoni8"
